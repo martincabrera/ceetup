9f4a5e486f97eb4c69e82ae44a1150ac812e81ce
Martin Cabrera <martin@kisland.com>
2018-01-30 00:35:59 +0100

    adds Topic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -15,5 +16,12 @@ City.find_or_create_by(name: 'Barcelona')
 City.find_or_create_by(name: 'Valencia')
 City.find_or_create_by(name: 'Seville')
 
+# Topics
 
-
+Topic.find_or_create_by(name: 'Back end')
+Topic.find_or_create_by(name: 'Front end')
+Topic.find_or_create_by(name: 'devops')
+Topic.find_or_create_by(name: 'development')
+Topic.find_or_create_by(name: 'Ruby')
+Topic.find_or_create_by(name: 'JavaScript')
+Topic.find_or_create_by(name: 'Java')
