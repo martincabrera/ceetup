a3cb6f9710822c139e206d9e067ac78905f89adb
Martin Cabrera <martin@kisland.com>
2018-01-30 00:35:29 +0100

    adds Topic model & migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_129_205_255) do
+ActiveRecord::Schema.define(version: 20_180_129_220_109) do
   create_table 'cities', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  create_table 'topics', force: :cascade do |t|
     t.string 'name'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
