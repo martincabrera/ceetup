8b323791402383dacb42797631b1e80d52a15fcb
Martin Cabrera <martin@kisland.com>
2018-02-01 00:37:26 +0100

    tweaks search_filter views & controller

--- app/controllers/search_filters_controller.rb
+++ app/controllers/search_filters_controller.rb
@@ -1,16 +1,16 @@
+# frozen_string_literal: true
+
 class SearchFiltersController < ApplicationController
-  before_action :set_search_filter, only: [:show, :edit, :update, :destroy]
+  before_action :set_search_filter, only: %i[show edit update destroy]
+  before_action :set_search_events, only: [:show]
 
   # GET /search_filters
-  # GET /search_filters.json
   def index
     @search_filters = SearchFilter.all
   end
 
   # GET /search_filters/1
-  # GET /search_filters/1.json
-  def show
-  end
+  def show; end
 
   # GET /search_filters/new
   def new
@@ -18,57 +18,46 @@ class SearchFiltersController < ApplicationController
   end
 
   # GET /search_filters/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /search_filters
-  # POST /search_filters.json
   def create
     @search_filter = SearchFilter.new(search_filter_params)
-
-    respond_to do |format|
-      if @search_filter.save
-        format.html { redirect_to @search_filter, notice: 'Search filter was successfully created.' }
-        format.json { render :show, status: :created, location: @search_filter }
-      else
-        format.html { render :new }
-        format.json { render json: @search_filter.errors, status: :unprocessable_entity }
-      end
+    if @search_filter.save
+      redirect_to @search_filter, notice: 'Search filter was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /search_filters/1
-  # PATCH/PUT /search_filters/1.json
   def update
-    respond_to do |format|
-      if @search_filter.update(search_filter_params)
-        format.html { redirect_to @search_filter, notice: 'Search filter was successfully updated.' }
-        format.json { render :show, status: :ok, location: @search_filter }
-      else
-        format.html { render :edit }
-        format.json { render json: @search_filter.errors, status: :unprocessable_entity }
-      end
+    if @search_filter.update(search_filter_params)
+      redirect_to @search_filter, notice: 'Search filter was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /search_filters/1
-  # DELETE /search_filters/1.json
   def destroy
     @search_filter.destroy
-    respond_to do |format|
-      format.html { redirect_to search_filters_url, notice: 'Search filter was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to search_filters_url, notice: 'Search filter was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_search_filter
-      @search_filter = SearchFilter.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def search_filter_params
-      params.require(:search_filter).permit(:user_id, :city_id, :start_date, :end_date, :topic_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_search_filter
+    @search_filter = SearchFilter.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def search_filter_params
+    params.require(:search_filter).permit(:user_id, :city_id, :start_date, :end_date, :topic_id)
+  end
+
+  def set_search_events
+    @events = Event.search(@search_filter.city_id, @search_filter.start_date, @search_filter.end_date, @search_filter.topic_id)
+  end
 end
