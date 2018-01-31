cfe485376626af4bcb0673974940c2b46ecae2fe
Martin Cabrera <martin@kisland.com>
2018-01-31 19:16:55 +0100

    adds specs to Event model

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :event do
     name 'Ruby meetup'
     start_date '2018-01-30 21:18:58'
-    end_date '2018-01-30 21:18:58'
+    end_date '2018-01-30 23:18:58'
     city nil
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,5 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'is not valid' do
+    it 'if name is empty' do
+      topic = build(:event, name: nil)
+      expect(topic).to_not be_valid
+    end
+
+    it 'if name is already taken in the same city' do
+      topic_name = 'Backend'
+      city = create(:city)
+      create(:event, name: topic_name, city_id: city.id )
+      second_topic = build(:event, name: topic_name, city_id: city.id)
+      expect(second_topic).to_not be_valid
+    end
+  end
+
+  describe 'is valid' do
+
+    let(:first_city) { create(:city) }
+    let(:second_city) { create(:city, name: 'Barcelona') }
+
+    it 'if name exits but in another city' do
+      topic_name = 'Backend'
+      create(:event, name: topic_name, city_id: city_id = first_city.id )
+      second_topic = build(:event, name: topic_name, city_id: second_city.id)
+      expect(second_topic).to be_valid
+    end
+  end
 end
