47abfc30ee80fafebb14a10fb228470fd670bd6d
Martin Cabrera <martin@kisland.com>
2018-02-01 19:36:01 +0100

    adds callback to notify user after creating an Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -18,6 +18,9 @@
 #
 
 class Event < ApplicationRecord
+  # callbacks
+  after_create :notify_users
+
   # associations
   belongs_to :city
   has_many :event_topics, dependent: :destroy
@@ -35,10 +38,19 @@ class Event < ApplicationRecord
   scope :in_date_range, ->(start_date, end_date) { where('start_date >= ? AND end_date <= ?', start_date, end_date) }
   scope :by_topic_id, ->(id) { joins(:event_topics).includes(:event_topics).where('event_topics.topic_id = ?', id) }
 
+  # delegations
+  delegate :name, to: :city, prefix: true
+
   def self.search(city_id, start_date, end_date, topic_id)
     scope = unscoped.by_city_id(city_id)
     scope = scope.in_date_range(start_date, end_date)
     scope = scope.by_topic_id(topic_id)
     scope
+  end
+
+  private
+
+  def notify_users
+    Notifier.new(self).notify
   end
 end
