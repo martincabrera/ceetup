4f626e7dea4d532bc2fcf94c074745e740099503
Martin Cabrera <martin@kisland.com>
2018-02-01 16:36:00 +0100

    fixes rubocop complaints

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,3 +1,5 @@
-Warden::Manager.before_logout do |user,auth,opts|
+# frozen_string_literal: true
+
+Warden::Manager.before_logout do |user, _auth, _opts|
   RedisConnection.connection.del(user.id)
 end
